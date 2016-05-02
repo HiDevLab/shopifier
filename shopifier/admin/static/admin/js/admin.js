057f73bbc5d05f0257664d5e72f430344bbc8e84
vkuryachenko <vkuryachenko@gmail.com>
2016-05-02 13:57:41 +0600

    Admin component

---
+++ shopifier/admin/static/admin/js/admin.js
@@ -0,0 +1,37 @@
+(function(app) {
+
+    app.Admin = ng.core.Component({
+        
+        "selector" : 'admin-form',
+        
+        "templateUrl" : "templates/admin.html",
+        
+        "directives" : [
+                        ng.common.FORM_DIRECTIVES,
+                        ng.router.ROUTER_DIRECTIVES,
+                        ],
+        "providers" : [
+                        ng.router.ROUTER_PROVIDERS, 
+                        ng.common.FormBuilder, 
+                        ng.common.Validators,
+                        app.AdminAuthService,
+                       
+                    ],
+    })
+    .Class({
+        constructor : [
+            app.AdminAuthService,
+            ng.router.Router,
+            ng.common.FormBuilder,
+                   
+            function(adminauthService, router, formbilder) {
+                this._adminauthService = adminauthService;
+                this._router = router;
+            }
+       
+        ],
+    });
+    
+    ng.router.CanActivate(() => app.CheckCurrenUser(true, 'Login'))(app.Admin);  
+
+})(window.app || (window.app = {}));
