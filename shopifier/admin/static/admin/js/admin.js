861f415ce76cace7c0a3c5fd77fe87f0d219d972
vkuryachenko <vkuryachenko@gmail.com>
2016-05-04 21:35:26 +0600

    Start menu

--- shopifier/admin/static/admin/js/admin.js
+++ shopifier/admin/static/admin/js/admin.js
@@ -1,7 +1,17 @@
-import { Component } from 'angular2/core';
+import { Component, Pipe, PipeTransform } from 'angular2/core';
 import { CanActivate } from 'angular2/router'
 
 import { getCurrentUser } from './admin.auth'
+import { Nav1, Nav2 } from './nav'
+
+@Pipe({name: 'parentNav'})
+export class ParentNavPipe {
+    transform(nav1, nav2){
+        return false;
+  }
+}
+
+
 
 @CanActivate(() => getCurrentUser(true, 'Login'))
 @Component({
@@ -9,4 +19,14 @@ import { getCurrentUser } from './admin.auth'
   templateUrl: 'templates/admin.html',
 })
 export class Admin {
+    navs1 = Nav1;
+    navs2 = Nav2;
+    selectedNav1 = Nav1[0];
+    displayNav2 = 'hide-class';
+    
+    onSelect1(nav1) {
+        this.selectedNav1 = nav1;
+        console.log(nav1);
+    }
 }
+
