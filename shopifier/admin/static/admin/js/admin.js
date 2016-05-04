874eec329771680e3eb43694734a0ccf8ef23295
vkuryachenko <vkuryachenko@gmail.com>
2016-05-04 23:09:56 +0600

    Admin main menu

--- shopifier/admin/static/admin/js/admin.js
+++ shopifier/admin/static/admin/js/admin.js
@@ -1,22 +1,22 @@
 import { Component, Pipe, PipeTransform } from 'angular2/core';
 import { CanActivate } from 'angular2/router'
+import { NgFor } from 'angular2/common';
 
 import { getCurrentUser } from './admin.auth'
 import { Nav1, Nav2 } from './nav'
 
 @Pipe({name: 'parentNav'})
 export class ParentNavPipe {
-    transform(nav1, nav2){
-        return false;
+    transform(nav2, selected_nav1){
+        return nav2.filter(nav2 => { return nav2.parent == selected_nav1.id; });
   }
 }
-
-
 
 @CanActivate(() => getCurrentUser(true, 'Login'))
 @Component({
   selector: 'admin-form',
   templateUrl: 'templates/admin.html',
+  pipes: [ParentNavPipe], 
 })
 export class Admin {
     navs1 = Nav1;
@@ -26,7 +26,7 @@ export class Admin {
     
     onSelect1(nav1) {
         this.selectedNav1 = nav1;
-        console.log(nav1);
+        
     }
 }
 
