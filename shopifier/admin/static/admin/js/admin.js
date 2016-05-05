4dbf0e00f174d48a65b61a927bcd794bc07a7289
andrew kuryachenko <andrew.kuryachenko@hidevlab.com>
2016-05-05 09:47:29 +0200

    CSS menu

--- shopifier/admin/static/admin/js/admin.js
+++ shopifier/admin/static/admin/js/admin.js
@@ -3,30 +3,20 @@ import { CanActivate } from 'angular2/router'
 import { NgFor } from 'angular2/common';
 
 import { getCurrentUser } from './admin.auth'
-import { Nav1, Nav2 } from './nav'
+import { Nav } from './nav'
 
-@Pipe({name: 'parentNav'})
-export class ParentNavPipe {
-    transform(nav2, selected_nav1){
-        return nav2.filter(nav2 => { return nav2.parent == selected_nav1.id; });
-  }
-}
 
 @CanActivate(() => getCurrentUser(true, 'Login'))
 @Component({
-  selector: 'admin-form',
-  templateUrl: 'templates/admin.html',
-  pipes: [ParentNavPipe], 
+    selector: 'admin-form',
+    templateUrl: 'templates/admin.html',
 })
 export class Admin {
-    navs1 = Nav1;
-    navs2 = Nav2;
-    selectedNav1 = Nav1[0];
-    displayNav2 = 'hide-class';
+    navs = Nav;
+    selectedNav = Nav[1];
+    //displayNav = 'hide-class';
     
-    onSelect1(nav1) {
-        this.selectedNav1 = nav1;
-        
+    onSelect(nav) {
+        this.selectedNav = nav;
     }
 }
-
--- shopifier/admin/static/admin/js/nav.js
+++ shopifier/admin/static/admin/js/nav.js
@@ -1,21 +1,52 @@
-export var Nav1 = [
-    {'id': 1, 'icon': 'fa fa-envelope',      'child': false,  'text': 'Search',    'url': '/'},
-    {'id': 2, 'icon': 'fa fa-envelope',      'child': false,  'text': 'Home',      'url': '/'},
-    {'id': 3, 'icon': 'fa fa-envelope',      'child': true,   'text': 'Orders',    'url': '/'},
-    {'id': 4, 'icon': 'fa fa-envelope',      'child': true,   'text': 'Products',  'url': '/'},
-    {'id': 5, 'icon': 'fa fa-envelope',      'child': false,  'text': 'Customers', 'url': '/'},
-    {'id': 6, 'icon': 'fa fa-envelope',      'child': false,  'text': 'Reports',   'url': '/'},
-    {'id': 7, 'icon': 'fa fa-envelope',      'child': false,  'text': 'Discounts', 'url': '/'},
-    {'id': 8, 'icon': 'fa fa-envelope',      'child': true,   'text': 'Settings',  'url': '/'}
-];
-
-export var Nav2 = [
-    {'id': 10, 'parent': 3, 'text': 'Orders',       'url': '/',     'parameters': {'action':''} },
-    {'id': 11, 'parent': 3, 'text': 'Drafts',       'url': '/',     'parameters': {'action':''} },
-    {'id': 12, 'parent': 4, 'text': 'Products',     'url': '/',     'parameters': {'action':''} },
-    {'id': 13, 'parent': 4, 'text': 'Transfers',    'url': '/',     'parameters': {'action':''} },
-    {'id': 14, 'parent': 8, 'text': 'General',      'url': '/',     'parameters': {'action':''} },
-    {'id': 15, 'parent': 8, 'text': 'Payments',     'url': '/',     'parameters': {'action':''} },
-    {'id': 16, 'parent': 8, 'text': 'Checkout',     'url': '/',     'parameters': {'action':''} },
-    {'id': 17, 'parent': 8, 'text': 'Shipping',     'url': '/',     'parameters': {'action':''} }, 
+export var Nav = [
+    {
+        'icon': 'search',
+        'text': 'Search',
+        'url': '#',
+        'submenu': [],
+    },
+    {
+        'icon': 'home',
+        'text': 'Home',
+        'url': '#',
+        'submenu': [],
+    },
+    {
+        'icon': 'calendar-check-o',
+        'text': 'Orders',
+        'url': '#',
+        'submenu': [
+             {
+                 'text': 'Orders',
+                 'url': '#',
+             },
+             {
+                 'text': 'Drafts',
+                 'url': '#',
+             },
+             {
+                 'text': 'Transfers',
+                 'url': '#',
+             }
+        ],
+    },
+    {
+        'icon': 'tag',
+        'text': 'Products',
+        'url': '#',
+        'submenu': [
+             {
+                 'text': 'Products',
+                 'url': '#',
+             },
+             {
+                 'text': 'Transfers',
+                 'url': '#',
+             },
+             {
+                 'text': 'Collections',
+                 'url': '#',
+             }
+        ],
+    }
 ];
