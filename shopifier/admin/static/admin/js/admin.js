84fbf06305c405f9dbcea22b8c1567f0babde4d2
vkuryachenko <vkuryachenko@gmail.com>
2016-05-06 10:45:06 +0600

    Nav animation

--- shopifier/admin/static/admin/js/admin.js
+++ shopifier/admin/static/admin/js/admin.js
@@ -1,6 +1,6 @@
 import { Component, Pipe, PipeTransform } from 'angular2/core';
 import { CanActivate } from 'angular2/router'
-import { NgFor } from 'angular2/common';
+import { FORM_DIRECTIVES, NgFor, ngIf } from 'angular2/common';
 
 import { getCurrentUser } from './admin.auth'
 import { Nav } from './nav'
@@ -9,6 +9,7 @@ import { Nav } from './nav'
 @Component({
     selector: 'admin-form',
     templateUrl: 'templates/admin.html',
+    directives    : [FORM_DIRECTIVES],
 })
 export class Admin {
     navs = Nav;
@@ -18,7 +19,10 @@ export class Admin {
      
     onSelect(nav) {
         this.selectedNav = nav;
+        this.forceSubmenuShow=true;
+        setTimeout(() => {this.forceSubmenuShow = false;}, 1000, this);   
     }
+    
     onSelectSubNav(nav) {
         this.selectedSubNav = nav;
     }
