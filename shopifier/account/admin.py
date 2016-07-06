586855f307587926d34986894e6ef57615a5cef8
vkuryachenko <vkuryachenko@gmail.com>
2016-07-06 13:37:33 +0700

    Pep8

--- shopifier/account/admin.py
+++ shopifier/account/admin.py
@@ -18,7 +18,7 @@ class UserLogInline(admin.TabularInline):
 class UserCreationForm(forms.ModelForm):
 
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation',   widget=forms.PasswordInput)
     
     class Meta:
         model = User
