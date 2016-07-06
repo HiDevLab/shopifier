8c25502b4bf1a7d0559a375138f4fcd993048116
vkuryachenko <vkuryachenko@gmail.com>
2016-07-06 19:54:05 +0700

    Syntax

--- shopifier/admin/static/admin/js/admin.boot.js
+++ shopifier/admin/static/admin/js/admin.boot.js
@@ -1,19 +1,19 @@
-import { bootstrap }    from 'angular2/platform/browser';
+import 'rxjs/Rx';
 
+import { bootstrap } from 'angular2/platform/browser';
+import { FORM_PROVIDERS, COMMON_DIRECTIVES } from 'angular2/common';
+import { Component, provide, Injectable, Injector } from 'angular2/core';
 import { ROUTER_BINDINGS, RouteConfig, Router, RouterOutlet, RouterLink, 
         ROUTER_PROVIDERS, ROUTER_DIRECTIVES } from 'angular2/router';
 import { HTTP_PROVIDERS, ConnectionBackend, Http, Headers, BaseRequestOptions, 
         Request, RequestOptions, XHRBackend,RequestMethod} from 'angular2/http';
-import { FORM_PROVIDERS, COMMON_DIRECTIVES } from 'angular2/common';
-import { Component, provide, Injectable, Injector } from 'angular2/core';
-import 'rxjs/Rx';
 
-import { AdminAuthService, AdminUtils, AdminAuthLogout, AdminAuthLogin, 
-        AdminAuthRecover, AdminAuthReset, AdminAuthAccept, AdminAuthWrongToken } 
-from './admin.auth'
+import { Admin } from './admin';
+import { AdminAuthService, AdminUtils, AdminAuthLogout,
+        AdminAuthLogin, AdminAuthRecover, AdminAuthReset,
+        AdminAuthAccept, AdminAuthWrongToken } from './admin.auth';
+import { Customers } from './admin.customers';
 
-import { Admin } from './admin'
-import { Customers } from './admin.customers' 
 
 @Component({
   selector: "body",
@@ -95,18 +95,19 @@ export class SuperHttp extends Http {
         this.requestoptions.headers = new Headers({
                     'Accept': 'application/json; charset=utf-8',
                     'Content-Type': 'application/json; charset=utf-8',
-                    'X-CSRFToken': '' 
+                    'X-CSRFToken': ''
                 });
     }
 
     csrfToken() {
-        let value = "; " + document.cookie;
-        let parts = value.split("; csrftoken=");
+        let value = '; ' + document.cookie;
+        let parts = value.split('; csrftoken=');
         if (parts.length == 2){ 
-            this.requestoptions.headers.set('X-CSRFToken', parts.pop().split(";").shift());
+            this.requestoptions
+                .headers.set('X-CSRFToken', parts.pop().split(";").shift());
         }
     }
-    
+
     request(method, url, data){
         this.csrfToken();
         this.requestoptions.method = RequestMethod[method];
@@ -129,13 +130,13 @@ export class SuperHttp extends Http {
     }
 
     get(url) {
-        return this.request('Get', url );
+        return this.request('Get', url);
     }
-     
+
     options(url) {
         return this.request('Options', url);
     }
-     
+
     delete(url) {
         this.csrfToken();
         return super.delete(url, {headers: this.requestoptions.headers});
@@ -148,7 +149,8 @@ bootstrap(AdminRouter, [
     HTTP_PROVIDERS,
     //provide( RequestOptions, { useClass: DefaultRequestOptions }),
     provide(Http, {
-        useFactory: (backend, defaultOptions) => new SuperHttp(backend, defaultOptions),
+        useFactory: (backend, defaultOptions) => 
+                        new SuperHttp(backend, defaultOptions),
         deps: [XHRBackend, RequestOptions]
     }),
     FORM_PROVIDERS,
